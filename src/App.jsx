286ac338e2ace386b78f03c687a1ae383bdcb978
Helena <helena@klarr.se>
2023-12-20 21:30:02 +0100

    test

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import imageOne from "/images/image_one.jpg";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          margin: "1rem",
+          width: "20rem",
+          height: "22rem",
+        }}
+      >
+        <img alt="house plant" src={imageOne} />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          margin: "1rem",
+          width: "20rem",
+          height: "22rem",
+        }}
+      >
+        <img alt="another house plant" src={"../public/images/image_two.jpg"} />
+      </div>
+    </>
+  );
+}
+
+export default App;
